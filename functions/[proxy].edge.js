0f7ee94b6f7867892e4d5f334aff6452f6e2a279
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2024-11-21 23:25:58 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -7,7 +7,10 @@ export default async function handler(request, context) {
     OAUTH_REDIRECT_URI: context.env.OAUTH_REDIRECT_URI,
     OAUTH_TOKEN_URL: context.env.OAUTH_TOKEN_URL
   };
-  
+
+  if (request.url.includes('/ttt')) {
+    return redirectTo('/')
+  }
   if (request.url.includes('_next') || request.url.includes('favicon.ico')) {
     return fetch(request);
   }
